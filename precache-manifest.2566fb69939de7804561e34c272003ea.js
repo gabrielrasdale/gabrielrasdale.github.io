952e5cb18e629d682a7559092bf92846ca305a35
Gabriel Rasdale <gabriel.rasdale@gmail.com>
2023-11-29 23:44:39 +0000

    landing page

---
+++ precache-manifest.2566fb69939de7804561e34c272003ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909e716fdb92e44206d795861c47f092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a1cbe05c2d255f1095",
+    "url": "/static/css/2.b661f99e.chunk.css"
+  },
+  {
+    "revision": "f3cb6b48ce19c52241e3",
+    "url": "/static/css/main.12baaab8.chunk.css"
+  },
+  {
+    "revision": "d5a1cbe05c2d255f1095",
+    "url": "/static/js/2.c3471963.chunk.js"
+  },
+  {
+    "revision": "d88ab20419b4dc86738c",
+    "url": "/static/js/3.6a5aac79.chunk.js"
+  },
+  {
+    "revision": "f3cb6b48ce19c52241e3",
+    "url": "/static/js/main.6b48a964.chunk.js"
+  },
+  {
+    "revision": "8ef0606bdd6fab043bc8",
+    "url": "/static/js/runtime~main.94dd3abe.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
